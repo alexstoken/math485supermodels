6cbfc4a564e25230aa76c262daa3771a60019f9e
Alex Stoken <astoken@email.arizona.edu>
2019-02-18 14:41:12 -0700

    added header of min_fn file

---
+++ min_fn.py
@@ -0,0 +1,10 @@
+"""
+Math 485 Spring 2019 Prof Lega
+Supermodel Group
+
+Created by: Alex Stoken 18 Feb 2019
+
+Last updated:
+
+This file takes input data and returns the minimum connection coefficient vector C
+"""
